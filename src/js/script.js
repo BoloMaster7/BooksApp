be28252f33f6390be416c418f8b4de57d14f09db
Pawel Godula <bolomaster7@gmail.com>
2022-10-12 18:57:50 +0200

    function render

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,30 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 {
-    'use strict';
-    const select = {
-        templateOf: {
-            book: '#template-book',
-        },
+  'use strict';
+  const select = {
+    templateOf: {
+      book: '#template-book',
     },
-        templates = {
-            books: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
-        };
+    containerOf: {
+      booksList: '.books-list',
+    },
+  };
+  const templates = {
+    books: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
+  };
 
-    function render() {
-        for (let book of dataSource.books) {
-            /* generate HTML based on template */
-            const generatedHTML = templates.books(book);
-            /* create element using utils.createElementFromHTML */
-            const element = utils.createDOMFromHTML(generatedHTML);
-            /* find menu container */
-            const menuContainer = document.querySelector(element);
-            /* add element to menu */
-            menuContainer.appendChild(element);
-        }
+
+  function render() {
+    for (let book of dataSource.books) {
+      /* generate HTML based on template */
+      const generatedHTML = templates.books(book);
+      /* create element using utils.createElementFromHTML */
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.booksList);
+      /* add element to menu */
+      menuContainer.appendChild(generatedDOM);
     }
-    render();
+  }
+  render();
 }
